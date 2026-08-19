1b75b8c899b8f4e834381fa9f045e27e1f0a9873
Luis E Gonzalez <luego1986@gmail.com>
2026-08-19 13:05:04 -0500

    feat: Add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import alpinejs from '@astrojs/alpinejs';
+import alpinejs from "@astrojs/alpinejs";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://whitescreenapp.online',
-  integrations: [alpinejs({ entrypoint: '/src/alpine' }), sitemap()],
+  site: "https://whitescreenapp.online",
+  integrations: [
+    alpinejs({ entrypoint: "/src/alpine" }),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
